1c98e4ba234bad1c78115b85463e57ec84623f5a
Immanuel Albrecht <github@genetic-superior.de>
2010-06-28 13:10:42 +0200

    Added module for handling equivalence constraints.

---
+++ eq_con.py
@@ -0,0 +1,29 @@
+# coding: utf-8
+#
+#   Copyright (C) 2010   C.D. Immanuel Albrecht
+#
+#   This program is free software: you can redistribute it and/or modify
+#   it under the terms of the GNU General Public License as published by
+#   the Free Software Foundation, either version 3 of the License, or
+#   (at your option) any later version.
+#
+#   This program is distributed in the hope that it will be useful,
+#   but WITHOUT ANY WARRANTY; without even the implied warranty of
+#   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#   GNU General Public License for more details.
+#
+#   You should have received a copy of the GNU General Public License
+#   along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+
+from __future__ import print_function
+
+class Constraint(object):
+    """Abstract class modelling a constraint"""
+    def __init__(self):
+        raise Exception('Constraint is an abstract class that cannot be'+\
+                        ' instantiated')
+
+    def __call__(self,object):
+        """Returns True, if object satisfies the constraint, False otherwise"""
+        raise Exception('Constraint satisfaction not defined!')
